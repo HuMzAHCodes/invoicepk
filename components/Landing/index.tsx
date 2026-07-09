a25861801dbcda8aae3571d0c78c6f3936fb6b10
HAmzaaider <kohat3066@gmail.com>
2026-07-09 16:34:59 +0500

    feat(landing): add Built for Pakistan section with 6 tax features and GST card

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import HowItWorks from "./HowItWorks";
+import BuiltForPakistan from "./BuiltForPakistan";
 
 export default function Landing() {
   return (
@@ -8,6 +9,7 @@ export default function Landing() {
       <Navbar />
       <Hero />
       <HowItWorks />
+      <BuiltForPakistan />
     </>
   );
 }
