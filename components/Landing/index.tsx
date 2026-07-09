fbb8fcbefa89bf96840d423da9addaf820ab05d9
HAmzaaider <kohat3066@gmail.com>
2026-07-09 18:15:48 +0500

    feat(landing): remove Built for Pakistan, move FAQ after How It Works

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,9 +1,8 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import HowItWorks from "./HowItWorks";
-import BuiltForPakistan from "./BuiltForPakistan";
-import Pricing from "./Pricing";
 import FAQ from "./FAQ";
+import Pricing from "./Pricing";
 import CTABanner from "./CTABanner";
 import Footer from "./Footer";
 
@@ -13,9 +12,8 @@ export default function Landing() {
       <Navbar />
       <Hero />
       <HowItWorks />
-      <BuiltForPakistan />
-      <Pricing />
       <FAQ />
+      <Pricing />
       <CTABanner />
       <Footer />
     </>
