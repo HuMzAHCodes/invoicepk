5b9cd0b199210e80f68220ff2bfff45979c08f92
HAmzaaider <kohat3066@gmail.com>
2026-07-10 09:34:34 +0500

    fix: move Firebase init into getAdmin to prevent build-time crash

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -3,22 +3,21 @@
 
 import admin from 'firebase-admin';
 
-if (!admin.apps.length) {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-  
-  const formattedKey = privateKey?.includes('\\n') 
-    ? privateKey.replace(/\\n/g, '\n')
-    : privateKey;
-
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId:   process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey:  formattedKey,
-    }),
-  });
-}
-
 export default function getAdmin() {
+  if (!admin.apps.length) {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
+    const formattedKey = privateKey?.includes('\\n')
+      ? privateKey.replace(/\\n/g, '\n')
+      : privateKey;
+
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId:   process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey:  formattedKey,
+      }),
+    });
+  }
   return admin;
 }
